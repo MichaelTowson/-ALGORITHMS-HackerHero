cbc2858faa24aa0de5ce62faa60ab0d701d2f78e
Michael Towson <MTowson@gmail.com>
2021-04-05 11:26:58 -0700

    add reverseArray file

---
+++ reverseArray.js
@@ -0,0 +1,11 @@
+//Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working ‘in-place’ means that you cannot use a second array – move values within the array that you are given. Do not use built-in functions.
+
+function reverse(arr){
+    
+}
+
+//TEST CASES
+console.log(reverse([1,3,5])) //[5,3,1]
+console.log(reverse([-1,3,-5])) //[-5,3,-1]
+console.log(reverse([1,2,3,4,5])) //[5,4,3,2,1]
+console.log(reverse([2,10,13,15])) //[15,13,10,2]
