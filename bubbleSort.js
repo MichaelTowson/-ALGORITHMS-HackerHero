1eaa56c5a48a4be67d8da21af69bb38b3a49bbe5
Michael Towson <mtowson@gmail.com>
2021-06-11 11:47:04 -0700

    add hackerHero solution to bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,3 +1,18 @@
+//HACKERHERO SOLUTION:
+function bubbleSort(arr) {
+    for(var j=0; j<arr.length;j++) {
+        for(var i=0; i<arr.length-1-j; i++) {
+            if(arr[i] > arr[i+1]) {
+                var temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+//MY ORIGINAL SOLUTION:
 function bubbleSort2(arr) {
     let temp;
 
